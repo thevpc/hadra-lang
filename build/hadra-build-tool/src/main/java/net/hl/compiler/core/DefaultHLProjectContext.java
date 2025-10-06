70a366aa2a8534fa602cd71240df980e93609ee8
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:25:18 +0100

    Update DefaultHLProjectContext.java : bump nuts 0.8.7

--- build/hadra-build-tool/src/main/java/net/hl/compiler/core/DefaultHLProjectContext.java
+++ build/hadra-build-tool/src/main/java/net/hl/compiler/core/DefaultHLProjectContext.java
@@ -1,9 +1,6 @@
 package net.hl.compiler.core;
 
 import net.hl.compiler.index.HIndexer;
-import net.thevpc.nuts.NSession;
-import net.thevpc.nuts.NWorkspace;
-import net.thevpc.nuts.Nuts;
 
 public class DefaultHLProjectContext implements HProjectContext {
 
