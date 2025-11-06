96bc10d7e133216b7a339e08b8971709cc3de7ab
thevpc <taha.bensalah@gmail.com>
2025-11-06 23:10:19 +0100

    Update JConstructorFromIndex.java : bump jeep

--- build/hadra-build-tool/src/main/java/net/hl/compiler/core/invokables/JConstructorFromIndex.java
+++ build/hadra-build-tool/src/main/java/net/hl/compiler/core/invokables/JConstructorFromIndex.java
@@ -8,100 +8,50 @@ import net.thevpc.jeep.impl.functions.JNameSignature;
 import net.thevpc.jeep.impl.functions.JSignature;
 import net.thevpc.jeep.impl.types.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
 
-public class JConstructorFromIndex extends AbstractJConstructor {
+public class JConstructorFromIndex extends DefaultJConstructor {
     private HIndexedConstructor ctr;
     private HIndexer indexer;
-    private JTypes types;
-    private JType declaringType;
 
     public JConstructorFromIndex(HIndexedConstructor ctr, JType declaringType, HIndexer indexer, JTypes types) {
-        this.declaringType = declaringType;
+        setDeclaringType(declaringType);
+        setSourceName(ctr.getSource());
+        setArgNames(ctr.getParameterNames());
         this.ctr = ctr;
         this.indexer = indexer;
-        this.types = types;
-    }
-
-    @Override
-    public JType getDeclaringType() {
-        return declaringType;
-    }
-
-    @Override
-    public String getName() {
-        return getDeclaringType().getName();
-    }
-
-    @Override
-    public JModifierList getModifiers() {
-        DefaultJModifierList e = new DefaultJModifierList();
-        for (AnnInfo annotation : ctr.getAnnotations()) {
-            if (annotation.getName().equals(annotation.getName().toLowerCase())) {
-                e.add(new JPrimitiveModifier(annotation.getName()));
+        modifiers.addSupplier(new Supplier<List<JModifier>>() {
+            @Override
+            public List<JModifier> get() {
+                List<JModifier> all=new ArrayList<JModifier>();
+                for (AnnInfo annotation : ctr.getAnnotations()) {
+                    if (annotation.getName().equals(annotation.getName().toLowerCase())) {
+                        all.add(new JPrimitiveModifier(annotation.getName()));
+                    }
+                }
+                return all;
             }
-        }
-        return e;
+        });
+        annotations.addSupplier(new  Supplier<List<JAnnotationInstance>>() {
+            @Override
+            public List<JAnnotationInstance> get() {
+                List<JAnnotationInstance> all=new ArrayList<>();
+                for (AnnInfo annotation : ctr.getAnnotations()) {
+                    all.add(new JAnnotationInstanceFromIndex(annotation, indexer, types));
+                }
+                return all;
+            }
+        });
+        setGenericSignature(JSignature.of(declaringType.getTypes(),"",ctr.getParameterTypes()));
     }
 
-    @Override
-    public JAnnotationInstanceList getAnnotations() {
-        DefaultJAnnotationInstanceList a = new DefaultJAnnotationInstanceList();
-        for (AnnInfo annotation : ctr.getAnnotations()) {
-            a.add(new JAnnotationInstanceFromIndex(annotation, indexer, types));
-        }
-        return a;
-    }
 
     @Override
     public Object invoke(JInvokeContext context) {
         throw new IllegalArgumentException("Not invokable, still source");
-    }
-
-    @Override
-    public String getSourceName() {
-        return ctr.getSource();
-    }
-
-    @Override
-    public JTypes getTypes() {
-        return types;
-    }
-
-    @Override
-    public String[] getArgNames() {
-        return ctr.getParameterNames();
-    }
-
-    @Override
-    public JType[] getArgTypes() {
-        return Arrays.stream(ctr.getParameterTypes())
-                .map(x->types.forName(x))
-                .toArray(JType[]::new);
-    }
-
-    @Override
-    public JTypeVariable[] getTypeParameters() {
-        return new JTypeVariable[0];
-    }
-
-    @Override
-    public JType getReturnType() {
-        return declaringType;
-    }
-
-    @Override
-    public JSignature getSignature() {
-        return JSignature.of(types,ctr.getSignature());
-    }
-
-    @Override
-    public JDeclaration getDeclaration() {
-        return null;
-    }
-
-    @Override
-    public JType getGenericReturnType() {
-        return super.getGenericReturnType();
     }
 }
