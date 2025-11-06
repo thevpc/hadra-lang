60b870dec22e862d477893b9ccd9af057a686828
thevpc <taha.bensalah@gmail.com>
2025-11-06 23:10:19 +0100

    Update JPrimitiveModifierAnnotationInstance.java : bump jeep

--- build/hadra-build-tool/src/main/java/net/hl/compiler/core/types/JPrimitiveModifierAnnotationInstance.java
+++ build/hadra-build-tool/src/main/java/net/hl/compiler/core/types/JPrimitiveModifierAnnotationInstance.java
@@ -1,10 +1,6 @@
 package net.hl.compiler.core.types;
 
-import net.thevpc.jeep.JAnnotationInstance;
-import net.thevpc.jeep.JAnnotationInstanceField;
 import net.thevpc.jeep.impl.types.DefaultJAnnotationInstance;
-
-import java.util.Objects;
 
 public class JPrimitiveModifierAnnotationInstance extends DefaultJAnnotationInstance {
 
