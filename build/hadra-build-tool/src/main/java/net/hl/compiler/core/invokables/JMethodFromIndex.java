fd7aeaa8f93cf58c387e0d8ec7593551715bcc49
thevpc <taha.bensalah@gmail.com>
2025-11-06 23:10:19 +0100

    Update JMethodFromIndex.java : bump jeep

--- build/hadra-build-tool/src/main/java/net/hl/compiler/core/invokables/JMethodFromIndex.java
+++ build/hadra-build-tool/src/main/java/net/hl/compiler/core/invokables/JMethodFromIndex.java
@@ -11,103 +11,49 @@ import net.thevpc.jeep.impl.types.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 
-public class JMethodFromIndex extends AbstractJMethod {
+public class JMethodFromIndex extends DefaultJRawMethod {
     private HIndexedMethod method;
     private HIndexer indexer;
-    private JTypes types;
-    private JType declaringType;
 
     public JMethodFromIndex(HIndexedMethod method, JType declaringType, HIndexer indexer, JTypes types) {
         this.method = method;
         this.indexer = indexer;
-        this.types = types;
-        this.declaringType = declaringType;
-    }
-
-    @Override
-    public JTypes getTypes() {
-        return types;
-    }
-
-    @Override
-    public JType getDeclaringType() {
-        return declaringType;
-    }
-
-    @Override
-    public JType[] getArgTypes() {
-        String[] ptypes = method.getParameterTypes();
-        List<JType> all = new ArrayList<>();
-        for (String jType : ptypes) {
-            all.add(types.forName(jType));
-        }
-        return all.toArray(new JType[0]);
-    }
-
-    @Override
-    public String[] getArgNames() {
-        return method.getParameterNames();
-    }
-
-    @Override
-    public boolean isDefault() {
-        return false;
-    }
-
-    @Override
-    public Object getDefaultValue() {
-        return null;
-    }
-
-    @Override
-    public JTypeVariable[] getTypeParameters() {
-        return new JTypeVariable[0];
-    }
-
-    @Override
-    public JMethod parametrize(JType... parameters) {
-        throw new JFixMeLaterException("JMethodFromIndex::parametrize");
+        setSourceName(method.getSource());
+        setDefaultMethod(false);
+        setDefaultValue(null);
+        setArgNames(method.getParameterNames());
+        setDeclaringType(declaringType);
+        setGenericReturnType(types.forName(method.getReturnType()));
+        setGenericSignature(JSignature.of(declaringType.getTypes(),method.getName(),method.getParameterTypes()));
+        modifiers.addSupplier(new Supplier<List<JModifier>>() {
+            @Override
+            public List<JModifier> get() {
+                List<JModifier> all=new ArrayList<JModifier>();
+                for (AnnInfo annotation : method.getAnnotations()) {
+                    if (annotation.getName().equals(annotation.getName().toLowerCase())) {
+                        all.add(new JPrimitiveModifier(annotation.getName()));
+                    }
+                }
+                return all;
+            }
+        });
+        annotations.addSupplier(new  Supplier<List<JAnnotationInstance>>() {
+            @Override
+            public List<JAnnotationInstance> get() {
+                List<JAnnotationInstance> all=new ArrayList<>();
+                for (AnnInfo annotation : method.getAnnotations()) {
+                    all.add(new JAnnotationInstanceFromIndex(annotation, indexer, types));
+                }
+                return all;
+            }
+        });
     }
 
     @Override
     public Object invoke(JInvokeContext context) {
         throw new IllegalArgumentException("Not invokable");
-    }
-
-    @Override
-    public JSignature getSignature() {
-        return JSignature.of(types, method.getSignature());
-    }
-
-    @Override
-    public JType getReturnType() {
-        return types.forName(method.getReturnType());
-    }
-
-    @Override
-    public JModifierList getModifiers() {
-        DefaultJModifierList e = new DefaultJModifierList();
-        for (AnnInfo annotation : method.getAnnotations()) {
-            if (annotation.getName().equals(annotation.getName().toLowerCase())) {
-                e.add(new JPrimitiveModifier(annotation.getName()));
-            }
-        }
-        return e;
-    }
-
-    @Override
-    public JAnnotationInstanceList getAnnotations() {
-        DefaultJAnnotationInstanceList a = new DefaultJAnnotationInstanceList();
-        for (AnnInfo annotation : method.getAnnotations()) {
-            a.add(new JAnnotationInstanceFromIndex(annotation, indexer, types));
-        }
-        return a;
-    }
-
-    @Override
-    public String getSourceName() {
-        return null;
     }
 
     @Override
